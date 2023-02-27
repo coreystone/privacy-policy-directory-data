0c968140ac7656ff9179ab020cbd5e4267084763
coreystone <24906602+coreystone@users.noreply.github.com>
2023-02-26 23:00:05 -0800

    Update format_md_table.py

--- format_md_table.py
+++ format_md_table.py
@@ -49,3 +49,32 @@ def cleanse_request_type(df) -> pd.DataFrame:
         i += 1
 
     return df
+
+
+def rename_columns(df) -> pd.DataFrame:
+    """ Adds a space between columns for readability """
+    return df.rename(columns={'PrivacyPolicy': 'Privacy Policy',
+                              'Request Form': 'Request Form',
+                              'RequestType': 'Request Type'
+                              })
+
+
+    if __name__ == '__main__':
+        input_file_path = 'input_file.csv'
+        output_file_path = 'output_file.csv'
+
+        print("Beginning program.")
+
+        try:
+            df = pd.read_csv(input_file_path)
+            df.fillna('', inplace=True)  # Replaces all NaN values with empty string for adding values
+        except:
+            print("Couldn't read .CSV file. Check formatting and try again.")
+            quit()
+
+        print("Markdown-ifying .csv file.")
+        df = emojify_frameworks(df)
+        df = emojify_links(df)
+        df = rename_columns(df)
+
+        df.to_csv(output_file_path, index=False) # Saves the result to CSV in output path
